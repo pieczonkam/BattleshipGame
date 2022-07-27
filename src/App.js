8c5029cb3fe5093bff040e65fa5da336d82b325c
pieczonkam <m3r3zz@gmail.com>
2022-07-27 17:08:45 +0200

    Add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import Board from './components/Board';
 import Card from './components/Card';
+import Navbar from './components/Navbar';
+import Pricing from './pages/Pricing';
+import About from './pages/About';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
+  
+  
   return (
-    <div className='App'>
-      <main className='flexbox'>
+    <>
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/pricing' element={<Pricing />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
+    </>
+
+    // <div className='App'>
+    //   <main className='flexbox'>
         
-        <Board id='board-1' className='board'>
-          <Card id='card-1' className='card' draggable='true'>
-            <p>Card one</p>
-          </Card>
-        </Board>
+        
+    //     <Board id='board-1' className='board'>
+    //       <Card id='card-1' className='card' draggable='true'>
+    //         <p>Card one</p>
+    //       </Card>
+    //     </Board>
 
-        <Board id='board-2' className='board'>
-          <Card id='card-2' className='card' draggable='true'>
-            <p>Card two</p>
-          </Card>
-        </Board>
+    //     <Board id='board-2' className='board'>
+    //       <Card id='card-2' className='card' draggable='true'>
+    //         <p>Card two</p>
+    //       </Card>
+    //     </Board>
 
-      </main>
-    </div>
+    //   </main>
+    // </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav className='nav'>
+            <Link to='/' className='site-title'>Site Name</Link>
+            <ul>
+                <CustomLink to='/pricing'>Pricing</CustomLink>
+                <CustomLink to='/about'>About</CustomLink>
+            </ul>
+        </nav>
+    );
+}
+
+function CustomLink({to, children, ...props}) {
+    const resolved_path = useResolvedPath(to);
+    const is_active = useMatch({ path: resolved_path.pathname, end: true });
+
+    
+    return (
+        <li className={is_active ? 'active' : ''}>
+            <Link to={to}>{children}</Link>
+        </li>
+    );
+}
+
+export default Navbar;
